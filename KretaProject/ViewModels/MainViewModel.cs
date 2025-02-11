c021a41718903ca910360579f2a23fa87de4a9f2
gycsaba <gycsaba@SZT9-24>
2025-02-11 13:47:58 +0100

    Menüpontok ViewModelei

--- KretaProject/ViewModels/MainViewModel.cs
+++ KretaProject/ViewModels/MainViewModel.cs
@@ -6,5 +6,17 @@ namespace KretaProject.ViewModels
     {
         [ObservableProperty]
         private object _currentView;
+
+        private readonly StudentViewModel _studentViewModel;
+        private readonly ControlPanelViewModel _controlPanelViewModel;
+        private readonly SchoolClassViewModel _schoolClassViewModel;
+
+        public MainViewModel()
+        {
+            _studentViewModel = new StudentViewModel();
+            _controlPanelViewModel = new ControlPanelViewModel();
+            _schoolClassViewModel = new SchoolClassViewModel();
+            CurrentView = _controlPanelViewModel;
+        }
     }
 }
